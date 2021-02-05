780b9b72e0dd7ac0ada07fbb5a6befff1ca039c1
豪宗超 <haozch@minstone.com.cn>
2021-02-05 11:44:33 +0800

    修复问题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "title": "HoweBlog",
-  "description": "Blog of HZC",
+  "title": "",
+  "description": "",
   "dest": "public",
   "head": [
     [
@@ -66,16 +66,7 @@ module.exports = {
       ]
     },
     "type": "blog",
-    "blogConfig": {
-      // "category": {
-      //   "location": 2,
-      //   "text": "Category"
-      // },
-      // "tag": {
-      //   "location": 3,
-      //   "text": "Tag"
-      // }
-    },
+    "blogConfig": {},
     "friendLink": [
       {
         "title": "午后南杂",
@@ -84,22 +75,22 @@ module.exports = {
         "link": "https://www.recoluan.com"
       },
       {
-        "title": "vuepress-theme-reco",
+        "title": "vuepress-html",
         "desc": "A simple and beautiful vuepress Blog & Doc theme.",
         "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
         "link": "https://vuepress-theme-reco.recoluan.com"
       }
     ],
-    "logo": "/avatar.jpg",//logo图标
+    "logo": "/avatar.jpg",
     "search": true,
     "searchMaxSuggestions": 10,
     "lastUpdated": "Last Updated",
-    "author": "howe916",
-    "authorAvatar": "/avatar.jpg",//个人头像
+    "author": "HZC",
+    "authorAvatar": "/avatar.jpg",
     "record": "@广东金融学院",
     "startYear": "2020"
   },
   "markdown": {
     "lineNumbers": true
   }
-};
+}
